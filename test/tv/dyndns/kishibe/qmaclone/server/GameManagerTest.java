66a640bef64a491028a32f22e04235246e01b768
Hisayori Noda <nodchip@gmail.com>
2014-11-02 12:24:41 +0900

    テスト修正

--- test/tv/dyndns/kishibe/qmaclone/server/GameManagerTest.java
+++ test/tv/dyndns/kishibe/qmaclone/server/GameManagerTest.java
@@ -24,178 +24,193 @@ import com.google.common.collect.Lists;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GameManagerTest {
-	@Mock
-	private Game.Factory mockGameFactory;
-	@Mock
-	private Game mockGame1, mockGame2, mockGame3, mockGame4;
-	@Mock
-	private ServerStatusManager mockServerStatusManager;
-	@Mock
-	private RestrictedUserUtils mockRestrictedUserUtils;
-	private GameManager gameManager;
+  @Mock
+  private Game.Factory mockGameFactory;
+  @Mock
+  private Game mockGame1, mockGame2, mockGame3, mockGame4;
+  @Mock
+  private ServerStatusManager mockServerStatusManager;
+  @Mock
+  private RestrictedUserUtils mockRestrictedUserUtils;
+  private GameManager gameManager;
 
-	@Before
-	public void setUp() throws Exception {
-		gameManager = new GameManager(mockGameFactory, mockRestrictedUserUtils);
-	}
+  @Before
+  public void setUp() throws Exception {
+    gameManager = new GameManager(mockGameFactory, mockRestrictedUserUtils);
+  }
 
-	@Test
-	public void getOrCreateMatchingSessionShouldReturnMatchingGameSession() {
-		when(mockGameFactory.create(1, 0, false, false, null, false)).thenReturn(mockGame1);
+  @Test
+  public void getOrCreateMatchingSessionShouldReturnMatchingGameSession() {
+    when(mockGameFactory.create(1, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame1);
 
-		Game game = gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
+    Game game = gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
 
-		assertSame(mockGame1, game);
-	}
+    assertSame(mockGame1, game);
+  }
 
-	@Test
-	public void getOrCreateMatchingSessionShouldReturnSameMatchingGameSessionForSecondTime() {
-		when(mockGameFactory.create(1, 0, false, false, null, false)).thenReturn(mockGame1);
-		when(mockGame1.getTransition()).thenReturn(Transition.Matching);
+  @Test
+  public void getOrCreateMatchingSessionShouldReturnSameMatchingGameSessionForSecondTime() {
+    when(mockGameFactory.create(1, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame1);
+    when(mockGame1.getTransition()).thenReturn(Transition.Matching);
 
-		List<Game> games = Lists.newArrayList();
+    List<Game> games = Lists.newArrayList();
 
-		for (int i = 0; i < 2; ++i) {
-			Game game = gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
-					EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-					mockServerStatusManager, 12345678, "192.168.0.1");
-			games.add(game);
-		}
+    for (int i = 0; i < 2; ++i) {
+      Game game = gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
+          EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+          mockServerStatusManager, 12345678, "192.168.0.1");
+      games.add(game);
+    }
 
-		assertEquals(ImmutableList.of(mockGame1, mockGame1), games);
-	}
+    assertEquals(ImmutableList.of(mockGame1, mockGame1), games);
+  }
 
-	@Test
-	public void getOrCreateMatchingSessionShouldReturnNewMatchingGameSessionIfFull() {
-		when(mockGameFactory.create(1, 0, false, false, null, false)).thenReturn(mockGame1);
-		when(mockGameFactory.create(2, 0, false, false, null, false)).thenReturn(mockGame2);
-		when(mockGame1.getTransition()).thenReturn(Transition.Matching, Transition.Matching,
-				Transition.Matching, Transition.Matching, Transition.Matching, Transition.Matching,
-				Transition.Matching, Transition.Ready);
+  @Test
+  public void getOrCreateMatchingSessionShouldReturnNewMatchingGameSessionIfFull() {
+    when(mockGameFactory.create(1, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame1);
+    when(mockGameFactory.create(2, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame2);
+    when(mockGame1.getTransition()).thenReturn(Transition.Matching, Transition.Matching,
+        Transition.Matching, Transition.Matching, Transition.Matching, Transition.Matching,
+        Transition.Matching, Transition.Ready);
 
-		List<Game> games = Lists.newArrayList();
+    List<Game> games = Lists.newArrayList();
 
-		for (int i = 0; i < 9; ++i) {
-			Game game = gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
-					EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-					mockServerStatusManager, 12345678, "192.168.0.1");
-			games.add(game);
-		}
+    for (int i = 0; i < 9; ++i) {
+      Game game = gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
+          EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+          mockServerStatusManager, 12345678, "192.168.0.1");
+      games.add(game);
+    }
 
-		assertEquals(ImmutableList.of(mockGame1, mockGame1, mockGame1, mockGame1, mockGame1,
-				mockGame1, mockGame1, mockGame1, mockGame2), games);
-	}
+    assertEquals(ImmutableList.of(mockGame1, mockGame1, mockGame1, mockGame1, mockGame1, mockGame1,
+        mockGame1, mockGame1, mockGame2), games);
+  }
 
-	@Test
-	public void getSessionShouldReturnCreatedGame() {
-		when(mockGameFactory.create(1, 0, false, false, null, false)).thenReturn(mockGame1);
+  @Test
+  public void getSessionShouldReturnCreatedGame() {
+    when(mockGameFactory.create(1, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame1);
 
-		gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
-		Game game = gameManager.getSession(1);
+    gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
+    Game game = gameManager.getSession(1);
 
-		assertSame(mockGame1, game);
-	}
+    assertSame(mockGame1, game);
+  }
 
-	@Test
-	public void getPublicMatchingEventRooms() {
-		when(mockGameFactory.create(1, 0, true, false, null, true)).thenReturn(mockGame1);
-		when(mockGame1.isEvent()).thenReturn(true);
-		when(mockGame1.isPublicEvent()).thenReturn(true);
-		when(mockGame1.getTransition()).thenReturn(Transition.Matching);
+  @Test
+  public void getPublicMatchingEventRooms() {
+    when(mockGameFactory.create(1, 0, true, false, null, true, GameMode.EVENT)).thenReturn(
+        mockGame1);
+    when(mockGame1.isEvent()).thenReturn(true);
+    when(mockGame1.isPublicEvent()).thenReturn(true);
+    when(mockGame1.getTransition()).thenReturn(Transition.Matching);
 
-		when(mockGameFactory.create(2, 0, true, false, null, false)).thenReturn(mockGame2);
-		when(mockGame2.isEvent()).thenReturn(true);
-		when(mockGame2.isPublicEvent()).thenReturn(false);
-		when(mockGame2.getTransition()).thenReturn(Transition.Matching);
+    when(mockGameFactory.create(2, 0, true, false, null, false, GameMode.EVENT)).thenReturn(
+        mockGame2);
+    when(mockGame2.isEvent()).thenReturn(true);
+    when(mockGame2.isPublicEvent()).thenReturn(false);
+    when(mockGame2.getTransition()).thenReturn(Transition.Matching);
 
-		when(mockGameFactory.create(3, 0, false, false, null, false)).thenReturn(mockGame3);
-		when(mockGame3.isEvent()).thenReturn(false);
-		when(mockGame3.isPublicEvent()).thenReturn(false);
-		when(mockGame3.getTransition()).thenReturn(Transition.Matching);
+    when(mockGameFactory.create(3, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame3);
+    when(mockGame3.isEvent()).thenReturn(false);
+    when(mockGame3.isPublicEvent()).thenReturn(false);
+    when(mockGame3.getTransition()).thenReturn(Transition.Matching);
 
-		when(mockGameFactory.create(4, 0, true, false, null, true)).thenReturn(mockGame3);
-		when(mockGame4.isEvent()).thenReturn(false);
-		when(mockGame4.isPublicEvent()).thenReturn(false);
-		when(mockGame4.getTransition()).thenReturn(Transition.Problem);
+    when(mockGameFactory.create(4, 0, true, false, null, true, GameMode.EVENT)).thenReturn(
+        mockGame3);
+    when(mockGame4.isEvent()).thenReturn(false);
+    when(mockGame4.isPublicEvent()).thenReturn(false);
+    when(mockGame4.getTransition()).thenReturn(Transition.Problem);
 
-		gameManager.getOrCreateMatchingSession(GameMode.EVENT, "public EVENT name", 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
-				mockServerStatusManager, 12345678, "192.168.0.1");
-		gameManager.getOrCreateMatchingSession(GameMode.EVENT, "closed EVENT name", 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
-		gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
-		gameManager.getOrCreateMatchingSession(GameMode.EVENT, "playing EVENT name", 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
-				mockServerStatusManager, 12345678, "192.168.0.1");
-		List<PacketRoomKey> rooms = gameManager.getPublicMatchingEventRooms();
+    gameManager.getOrCreateMatchingSession(GameMode.EVENT, "public EVENT name", 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
+        mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.EVENT, "closed EVENT name", 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.EVENT, "playing EVENT name", 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
+        mockServerStatusManager, 12345678, "192.168.0.1");
+    List<PacketRoomKey> rooms = gameManager.getPublicMatchingEventRooms();
 
-		assertEquals(ImmutableList.of(new PacketRoomKey(GameMode.EVENT, "public EVENT name",
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu))), rooms);
-	}
+    assertEquals(ImmutableList.of(new PacketRoomKey(GameMode.EVENT, "public EVENT name", EnumSet
+        .of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu))), rooms);
+  }
 
-	@Test
-	public void getNumberOfPlayersInWholeShouldReturnNumberOfHumanPlayers() {
-		when(mockGameFactory.create(1, 0, false, false, null, false)).thenReturn(mockGame1);
-		when(mockGame1.getNumberOfHumanPlayer()).thenReturn(3);
-		when(mockGame1.getTransition()).thenReturn(Transition.Matching);
+  @Test
+  public void getNumberOfPlayersInWholeShouldReturnNumberOfHumanPlayers() {
+    when(mockGameFactory.create(1, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame1);
+    when(mockGame1.getNumberOfHumanPlayer()).thenReturn(3);
+    when(mockGame1.getTransition()).thenReturn(Transition.Matching);
 
-		gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
 
-		assertEquals(3, gameManager.getNumberOfPlayersInWhole());
-	}
+    assertEquals(3, gameManager.getNumberOfPlayersInWhole());
+  }
 
-	@Test
-	public void getNumberOfSessionsShouldReturnSizeOfSessions() {
-		when(mockGameFactory.create(1, 0, true, false, null, true)).thenReturn(mockGame1);
-		when(mockGameFactory.create(2, 0, true, false, null, false)).thenReturn(mockGame2);
-		when(mockGameFactory.create(3, 0, false, false, null, false)).thenReturn(mockGame3);
+  @Test
+  public void getNumberOfSessionsShouldReturnSizeOfSessions() {
+    when(mockGameFactory.create(1, 0, true, false, null, true, GameMode.EVENT)).thenReturn(
+        mockGame1);
+    when(mockGameFactory.create(2, 0, true, false, null, false, GameMode.EVENT)).thenReturn(
+        mockGame2);
+    when(mockGameFactory.create(3, 0, false, false, null, false, GameMode.WHOLE)).thenReturn(
+        mockGame3);
 
-		assertEquals(0, gameManager.getNumberOfSessions());
+    assertEquals(0, gameManager.getNumberOfSessions());
 
-		gameManager.getOrCreateMatchingSession(GameMode.EVENT, "public EVENT name", 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
-				mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.EVENT, "public EVENT name", 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
+        mockServerStatusManager, 12345678, "192.168.0.1");
 
-		assertEquals(1, gameManager.getNumberOfSessions());
+    assertEquals(1, gameManager.getNumberOfSessions());
 
-		gameManager.getOrCreateMatchingSession(GameMode.EVENT, "closed EVENT name", 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.EVENT, "closed EVENT name", 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
 
-		assertEquals(2, gameManager.getNumberOfSessions());
+    assertEquals(2, gameManager.getNumberOfSessions());
 
-		gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.WHOLE, null, 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
 
-		assertEquals(3, gameManager.getNumberOfSessions());
-	}
+    assertEquals(3, gameManager.getNumberOfSessions());
+  }
 
-	@Test
-	public void getNumberOfPlayersShouldReturnTotalNumberOfHumanPlayers() {
-		when(mockGameFactory.create(1, 0, true, false, null, true)).thenReturn(mockGame1);
-		when(mockGame1.getNumberOfHumanPlayer()).thenReturn(1);
+  @Test
+  public void getNumberOfPlayersShouldReturnTotalNumberOfHumanPlayers() {
+    when(mockGameFactory.create(1, 0, true, false, null, true, GameMode.EVENT)).thenReturn(
+        mockGame1);
+    when(mockGame1.getNumberOfHumanPlayer()).thenReturn(1);
 
-		when(mockGameFactory.create(2, 0, true, false, null, false)).thenReturn(mockGame2);
-		when(mockGame2.getNumberOfHumanPlayer()).thenReturn(2);
+    when(mockGameFactory.create(2, 0, true, false, null, false, GameMode.EVENT)).thenReturn(
+        mockGame2);
+    when(mockGame2.getNumberOfHumanPlayer()).thenReturn(2);
 
-		gameManager.getOrCreateMatchingSession(GameMode.EVENT, "public EVENT name", 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
-				mockServerStatusManager, 12345678, "192.168.0.1");
-		gameManager.getOrCreateMatchingSession(GameMode.EVENT, "closed EVENT name", 0, null,
-				EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
-				mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.EVENT, "public EVENT name", 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), true,
+        mockServerStatusManager, 12345678, "192.168.0.1");
+    gameManager.getOrCreateMatchingSession(GameMode.EVENT, "closed EVENT name", 0, null,
+        EnumSet.of(ProblemGenre.Anige), EnumSet.of(ProblemType.Marubatsu), false,
+        mockServerStatusManager, 12345678, "192.168.0.1");
 
-		assertEquals(3, gameManager.getNumberOfPlayers());
-	}
+    assertEquals(3, gameManager.getNumberOfPlayers());
+  }
 
 }
--- test/tv/dyndns/kishibe/qmaclone/server/GameTest.java
+++ test/tv/dyndns/kishibe/qmaclone/server/GameTest.java
@@ -19,6 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import tv.dyndns.kishibe.qmaclone.client.game.GameMode;
 import tv.dyndns.kishibe.qmaclone.client.game.ProblemGenre;
 import tv.dyndns.kishibe.qmaclone.client.game.ProblemType;
 import tv.dyndns.kishibe.qmaclone.client.packet.NewAndOldProblems;
@@ -34,132 +35,131 @@ import com.google.inject.Inject;
 @RunWith(JUnit4.class)
 public class GameTest extends EasyMockSupport {
 
-	private static final String FAKE_REMOTE_ADDRESS = "1.2.3.4";
-	@Rule
-	public final GuiceBerryRule rule = new GuiceBerryRule(QMACloneTestEnv.class);
-	@Inject
-	private Game.Factory gameFactory;
-	private Game game;
+  private static final String FAKE_REMOTE_ADDRESS = "1.2.3.4";
+  @Rule
+  public final GuiceBerryRule rule = new GuiceBerryRule(QMACloneTestEnv.class);
+  @Inject
+  private Game.Factory gameFactory;
+  private Game game;
 
-	@Before
-	public void setUp() throws Exception {
-		game = gameFactory.create(0, 0, false, false, null, false);
-	}
+  @Before
+  public void setUp() throws Exception {
+    game = gameFactory.create(0, 0, false, false, null, false, GameMode.WHOLE);
+  }
 
-	@Test
-	public void testCalculateRating() {
-		PlayerStatus player1 = new PlayerStatus(null, 1, 0, 0, true, null, null, 0, 1800, 10000000,
-				300, 100);
-		player1.addScore(5000);
-		PlayerStatus player2 = new PlayerStatus(null, 2, 0, 0, true, null, null, 0, 1600, 10000001,
-				300, 200);
-		player2.addScore(4000);
-		PlayerStatus player3 = new PlayerStatus(null, 3, 0, 0, true, null, null, 0, 1400, 10000002,
-				300, 300);
-		player3.addScore(3000);
-		PlayerStatus player4 = new PlayerStatus(null, 4, 0, 0, true, null, null, 0, 1200, 10000003,
-				300, 400);
-		player4.addScore(2000);
-		PlayerStatus player5 = new PlayerStatus(null, 5, 0, 0, true, null, null, 0, 1000, 10000004,
-				300, 500);
-		player5.addScore(1000);
-		ArrayList<PlayerStatus> list = Lists.newArrayList(player1, player2, player3, player4,
-				player5);
+  @Test
+  public void testCalculateRating() {
+    PlayerStatus player1 = new PlayerStatus(null, 1, 0, 0, true, null, null, 0, 1800, 10000000,
+        300, 100);
+    player1.addScore(5000);
+    PlayerStatus player2 = new PlayerStatus(null, 2, 0, 0, true, null, null, 0, 1600, 10000001,
+        300, 200);
+    player2.addScore(4000);
+    PlayerStatus player3 = new PlayerStatus(null, 3, 0, 0, true, null, null, 0, 1400, 10000002,
+        300, 300);
+    player3.addScore(3000);
+    PlayerStatus player4 = new PlayerStatus(null, 4, 0, 0, true, null, null, 0, 1200, 10000003,
+        300, 400);
+    player4.addScore(2000);
+    PlayerStatus player5 = new PlayerStatus(null, 5, 0, 0, true, null, null, 0, 1000, 10000004,
+        300, 500);
+    player5.addScore(1000);
+    ArrayList<PlayerStatus> list = Lists.newArrayList(player1, player2, player3, player4, player5);
 
-		game.calculateRating(list);
+    game.calculateRating(list);
 
-		assertThat(player1.getNewRating(), greaterThan(1800));
-		assertThat(player2.getNewRating(), greaterThan(1600));
-		assertThat(player4.getNewRating(), lessThan(1200));
-		assertThat(player5.getNewRating(), lessThan(1000));
-	}
+    assertThat(player1.getNewRating(), greaterThan(1800));
+    assertThat(player2.getNewRating(), greaterThan(1600));
+    assertThat(player4.getNewRating(), lessThan(1200));
+    assertThat(player5.getNewRating(), lessThan(1000));
+  }
 
-	@Test
-	public void testCalculateRating2() {
-		PlayerStatus player1 = new PlayerStatus(null, 1, 0, 0, true, null, null, 0, 1274, 10000000,
-				300, 100);
-		player1.addScore(5000);
-		PlayerStatus player2 = new PlayerStatus(null, 2, 0, 0, true, null, null, 0, 1274, 10000001,
-				300, 200);
-		player2.addScore(4000);
-		ArrayList<PlayerStatus> list = Lists.newArrayList(player1, player2);
+  @Test
+  public void testCalculateRating2() {
+    PlayerStatus player1 = new PlayerStatus(null, 1, 0, 0, true, null, null, 0, 1274, 10000000,
+        300, 100);
+    player1.addScore(5000);
+    PlayerStatus player2 = new PlayerStatus(null, 2, 0, 0, true, null, null, 0, 1274, 10000001,
+        300, 200);
+    player2.addScore(4000);
+    ArrayList<PlayerStatus> list = Lists.newArrayList(player1, player2);
 
-		game.calculateRating(list);
+    game.calculateRating(list);
 
-		assertThat(player1.getNewRating(), greaterThan(1274));
-		assertThat(player2.getNewRating(), lessThan(1274));
-	}
+    assertThat(player1.getNewRating(), greaterThan(1274));
+    assertThat(player2.getNewRating(), lessThan(1274));
+  }
 
-	@Test
-	public void testCalculateRating3() {
-		PlayerStatus player1 = new PlayerStatus(null, 1, 0, 0, true, null, null, 0, 1274, 10000000,
-				300, 100);
-		player1.addScore(5000);
-		PlayerStatus player2 = new PlayerStatus(null, 2, 0, 0, true, null, null, 0, 1274, 10000001,
-				300, 200);
-		player2.addScore(5000);
-		ArrayList<PlayerStatus> list = Lists.newArrayList(player1, player2);
+  @Test
+  public void testCalculateRating3() {
+    PlayerStatus player1 = new PlayerStatus(null, 1, 0, 0, true, null, null, 0, 1274, 10000000,
+        300, 100);
+    player1.addScore(5000);
+    PlayerStatus player2 = new PlayerStatus(null, 2, 0, 0, true, null, null, 0, 1274, 10000001,
+        300, 200);
+    player2.addScore(5000);
+    ArrayList<PlayerStatus> list = Lists.newArrayList(player1, player2);
 
-		game.calculateRating(list);
+    game.calculateRating(list);
 
-		// assertEquals(1274, player1.getNewRating());
-		// assertEquals(1274, player2.getNewRating());
-	}
+    // assertEquals(1274, player1.getNewRating());
+    // assertEquals(1274, player2.getNewRating());
+  }
 
-	@Test
-	public void prepareProblemsShouldSelectSameGenreAndTypeIfOnlyOneIsSelected() {
-		List<Integer> problemIds = Lists.newArrayList();
-		Set<ProblemGenre> selectedGenres = EnumSet.of(ProblemGenre.Anige);
-		Set<ProblemType> selectedTypes = EnumSet.of(ProblemType.Marubatsu);
+  @Test
+  public void prepareProblemsShouldSelectSameGenreAndTypeIfOnlyOneIsSelected() {
+    List<Integer> problemIds = Lists.newArrayList();
+    Set<ProblemGenre> selectedGenres = EnumSet.of(ProblemGenre.Anige);
+    Set<ProblemType> selectedTypes = EnumSet.of(ProblemType.Marubatsu);
 
-		List<PacketProblem> problems = game.prepareProblems(0, NewAndOldProblems.Both, problemIds,
-				selectedGenres, selectedTypes, 0, null);
+    List<PacketProblem> problems = game.prepareProblems(0, NewAndOldProblems.Both, problemIds,
+        selectedGenres, selectedTypes, 0, null);
 
-		for (PacketProblem problem : problems) {
-			assertEquals(ProblemGenre.Anige, problem.genre);
-			assertEquals(ProblemType.Marubatsu, problem.type);
-		}
-	}
+    for (PacketProblem problem : problems) {
+      assertEquals(ProblemGenre.Anige, problem.genre);
+      assertEquals(ProblemType.Marubatsu, problem.type);
+    }
+  }
 
-	@Test
-	public void prepareProblemsShouldSelectFromSelectedGenresAndTypesEqually() {
-		List<Integer> problemIds = Lists.newArrayList();
-		Set<ProblemGenre> selectedGenres = EnumSet.of(ProblemGenre.Anige, ProblemGenre.Sports);
-		Set<ProblemType> selectedTypes = EnumSet.of(ProblemType.Marubatsu, ProblemType.YonTaku,
-				ProblemType.Rensou);
+  @Test
+  public void prepareProblemsShouldSelectFromSelectedGenresAndTypesEqually() {
+    List<Integer> problemIds = Lists.newArrayList();
+    Set<ProblemGenre> selectedGenres = EnumSet.of(ProblemGenre.Anige, ProblemGenre.Sports);
+    Set<ProblemType> selectedTypes = EnumSet.of(ProblemType.Marubatsu, ProblemType.YonTaku,
+        ProblemType.Rensou);
 
-		List<PacketProblem> problems = game.prepareProblems(0, NewAndOldProblems.Both, problemIds,
-				selectedGenres, selectedTypes, 0, null);
+    List<PacketProblem> problems = game.prepareProblems(0, NewAndOldProblems.Both, problemIds,
+        selectedGenres, selectedTypes, 0, null);
 
-		Multiset<ProblemGenre> genres = EnumMultiset.create(ProblemGenre.class);
-		Multiset<ProblemType> types = EnumMultiset.create(ProblemType.class);
+    Multiset<ProblemGenre> genres = EnumMultiset.create(ProblemGenre.class);
+    Multiset<ProblemType> types = EnumMultiset.create(ProblemType.class);
 
-		for (PacketProblem problem : problems) {
-			assertThat(problem.genre, isIn(selectedGenres));
-			genres.add(problem.genre);
+    for (PacketProblem problem : problems) {
+      assertThat(problem.genre, isIn(selectedGenres));
+      genres.add(problem.genre);
 
-			assertThat(problem.type, isIn(selectedTypes));
-			types.add(problem.type);
-		}
+      assertThat(problem.type, isIn(selectedTypes));
+      types.add(problem.type);
+    }
 
-		// 問題が見つからなかった場合に問題数が少なくなる場合があることを許容する
-		assertThat(genres.count(ProblemGenre.Anige), isOneOf(7, 8, 9));
-		assertThat(genres.count(ProblemGenre.Sports), isOneOf(7, 8, 9));
-		assertThat(types.count(ProblemType.Marubatsu), isOneOf(3, 4, 5, 6, 7));
-		assertThat(types.count(ProblemType.YonTaku), isOneOf(3, 4, 5, 6, 7));
-		assertThat(types.count(ProblemType.Rensou), isOneOf(3, 4, 5, 6, 7));
-	}
+    // 問題が見つからなかった場合に問題数が少なくなる場合があることを許容する
+    assertThat(genres.count(ProblemGenre.Anige), isOneOf(7, 8, 9));
+    assertThat(genres.count(ProblemGenre.Sports), isOneOf(7, 8, 9));
+    assertThat(types.count(ProblemType.Marubatsu), isOneOf(3, 4, 5, 6, 7));
+    assertThat(types.count(ProblemType.YonTaku), isOneOf(3, 4, 5, 6, 7));
+    assertThat(types.count(ProblemType.Rensou), isOneOf(3, 4, 5, 6, 7));
+  }
 
-	@Test
-	public void prepareProblemsShouldSelectEvenIfGenresAndTypesAreEmpty() {
-		// テーマモードではジャンルとタイプは空で送られてくる
-		List<Integer> problemIds = Lists.newArrayList();
-		Set<ProblemGenre> selectedGenres = EnumSet.noneOf(ProblemGenre.class);
-		Set<ProblemType> selectedTypes = EnumSet.noneOf(ProblemType.class);
+  @Test
+  public void prepareProblemsShouldSelectEvenIfGenresAndTypesAreEmpty() {
+    // テーマモードではジャンルとタイプは空で送られてくる
+    List<Integer> problemIds = Lists.newArrayList();
+    Set<ProblemGenre> selectedGenres = EnumSet.noneOf(ProblemGenre.class);
+    Set<ProblemType> selectedTypes = EnumSet.noneOf(ProblemType.class);
 
-		List<PacketProblem> problems = game.prepareProblems(0, NewAndOldProblems.Both, problemIds,
-				selectedGenres, selectedTypes, 0, null);
+    List<PacketProblem> problems = game.prepareProblems(0, NewAndOldProblems.Both, problemIds,
+        selectedGenres, selectedTypes, 0, null);
 
-		assertEquals(16, problems.size());
-	}
+    assertEquals(16, problems.size());
+  }
 }
