aa66d8c70a561d9da9374f30547f7c186bc984b7
Hisayori Noda <nodchip@gmail.com>
2015-02-15 14:02:35 +0900

    Deprecatedとなっているクラスから移行した

--- src/tv/dyndns/kishibe/qmaclone/server/relevance/NGramAnalyzer.java
+++ src/tv/dyndns/kishibe/qmaclone/server/relevance/NGramAnalyzer.java
@@ -3,22 +3,20 @@ package tv.dyndns.kishibe.qmaclone.server.relevance;
 import java.io.Reader;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.ngram.Lucene43NGramTokenizer;
+import org.apache.lucene.analysis.ngram.NGramTokenizer;
 
 public class NGramAnalyzer extends Analyzer {
 
-	public static final int MAX_NGRAM_WEIGHT = 5;
-	private static final int MIN_NGRAM_WEIGHT = 1;
+  public static final int MAX_NGRAM_WEIGHT = 5;
+  private static final int MIN_NGRAM_WEIGHT = 1;
 
-	@Override
-	protected Reader initReader(String fieldName, Reader reader) {
-		return ReaderUtil.wrapWithNormalizer(reader);
-	}
+  @Override
+  protected Reader initReader(String fieldName, Reader reader) {
+    return ReaderUtil.wrapWithNormalizer(reader);
+  }
 
-	@Override
-	protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
-		return new TokenStreamComponents(new Lucene43NGramTokenizer(reader, MIN_NGRAM_WEIGHT,
-				MAX_NGRAM_WEIGHT));
-	}
-
+  @Override
+  protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+    return new TokenStreamComponents(new NGramTokenizer(reader, MIN_NGRAM_WEIGHT, MAX_NGRAM_WEIGHT));
+  }
 }
