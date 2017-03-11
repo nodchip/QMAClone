3e223972fa766f9d3e1fe92ff40a5f5ec367684a
Hisayori Noda <nodchip@gmail.com>
2017-03-11 17:43:45 +0900

    スレッドプールのスレッド数を論理コア数-1に変更した

--- src/tv/dyndns/kishibe/qmaclone/server/ThreadPool.java
+++ src/tv/dyndns/kishibe/qmaclone/server/ThreadPool.java
@@ -37,121 +37,121 @@ import java.util.concurrent.TimeoutException;
 import org.apache.lucene.util.NamedThreadFactory;
 
 public class ThreadPool implements ScheduledExecutorService {
-	private static final int numberOfThreads = 8;
-	private final ScheduledExecutorService scheduledExecutorService = Executors
-			.newScheduledThreadPool(numberOfThreads, new NamedThreadFactory("ThreadPool-scheduled"));
-	private final ExecutorService cachedExecutorService = Executors
-			.newCachedThreadPool(new NamedThreadFactory("ThreadPool-cached"));
+  private final ScheduledExecutorService scheduledExecutorService = Executors
+      .newScheduledThreadPool(Math.max(1, Runtime.getRuntime().availableProcessors() - 1),
+          new NamedThreadFactory("ThreadPool-scheduled"));
+  private final ExecutorService cachedExecutorService = Executors
+      .newCachedThreadPool(new NamedThreadFactory("ThreadPool-cached"));
 
-	@Override
-	public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-		return scheduledExecutorService.awaitTermination(timeout, unit)
-				&& cachedExecutorService.awaitTermination(timeout, unit);
-	}
+  @Override
+  public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+    return scheduledExecutorService.awaitTermination(timeout, unit)
+        && cachedExecutorService.awaitTermination(timeout, unit);
+  }
 
-	@Override
-	public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
-			throws InterruptedException {
-		return cachedExecutorService.invokeAll(tasks);
-	}
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
+      throws InterruptedException {
+    return cachedExecutorService.invokeAll(tasks);
+  }
 
-	@Override
-	public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout,
-			TimeUnit unit) throws InterruptedException {
-		return cachedExecutorService.invokeAll(tasks, timeout, unit);
-	}
+  @Override
+  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout,
+      TimeUnit unit) throws InterruptedException {
+    return cachedExecutorService.invokeAll(tasks, timeout, unit);
+  }
 
-	@Override
-	public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException,
-			ExecutionException {
-		return cachedExecutorService.invokeAny(tasks);
-	}
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks)
+      throws InterruptedException, ExecutionException {
+    return cachedExecutorService.invokeAny(tasks);
+  }
 
-	@Override
-	public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-			throws InterruptedException, ExecutionException, TimeoutException {
-		return cachedExecutorService.invokeAny(tasks, timeout, unit);
-	}
+  @Override
+  public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
+      throws InterruptedException, ExecutionException, TimeoutException {
+    return cachedExecutorService.invokeAny(tasks, timeout, unit);
+  }
 
-	@Override
-	public boolean isShutdown() {
-		return scheduledExecutorService.isShutdown() && cachedExecutorService.isShutdown();
-	}
+  @Override
+  public boolean isShutdown() {
+    return scheduledExecutorService.isShutdown() && cachedExecutorService.isShutdown();
+  }
 
-	@Override
-	public boolean isTerminated() {
-		return scheduledExecutorService.isTerminated() && cachedExecutorService.isTerminated();
-	}
+  @Override
+  public boolean isTerminated() {
+    return scheduledExecutorService.isTerminated() && cachedExecutorService.isTerminated();
+  }
 
-	@Override
-	public void shutdown() {
-		scheduledExecutorService.shutdown();
-		cachedExecutorService.shutdown();
-	}
+  @Override
+  public void shutdown() {
+    scheduledExecutorService.shutdown();
+    cachedExecutorService.shutdown();
+  }
 
-	@Override
-	public List<Runnable> shutdownNow() {
-		final List<Runnable> runnables = new ArrayList<Runnable>();
-		runnables.addAll(scheduledExecutorService.shutdownNow());
-		runnables.addAll(cachedExecutorService.shutdownNow());
-		return runnables;
-	}
+  @Override
+  public List<Runnable> shutdownNow() {
+    final List<Runnable> runnables = new ArrayList<Runnable>();
+    runnables.addAll(scheduledExecutorService.shutdownNow());
+    runnables.addAll(cachedExecutorService.shutdownNow());
+    return runnables;
+  }
 
-	@Override
-	public <T> Future<T> submit(Callable<T> task) {
-		return cachedExecutorService.submit(task);
-	}
+  @Override
+  public <T> Future<T> submit(Callable<T> task) {
+    return cachedExecutorService.submit(task);
+  }
 
-	@Override
-	public Future<?> submit(Runnable task) {
-		return cachedExecutorService.submit(task);
-	}
+  @Override
+  public Future<?> submit(Runnable task) {
+    return cachedExecutorService.submit(task);
+  }
 
-	@Override
-	public <T> Future<T> submit(Runnable task, T result) {
-		return cachedExecutorService.submit(task, result);
-	}
+  @Override
+  public <T> Future<T> submit(Runnable task, T result) {
+    return cachedExecutorService.submit(task, result);
+  }
 
-	@Override
-	public void execute(Runnable command) {
-		cachedExecutorService.execute(command);
-	}
+  @Override
+  public void execute(Runnable command) {
+    cachedExecutorService.execute(command);
+  }
 
-	@Override
-	public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-		return scheduledExecutorService.schedule(command, delay, unit);
-	}
+  @Override
+  public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+    return scheduledExecutorService.schedule(command, delay, unit);
+  }
 
-	@Override
-	public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
-		return scheduledExecutorService.schedule(callable, delay, unit);
-	}
+  @Override
+  public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+    return scheduledExecutorService.schedule(callable, delay, unit);
+  }
 
-	@Override
-	public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period,
-			TimeUnit unit) {
-		return scheduledExecutorService.scheduleAtFixedRate(command, initialDelay, period, unit);
-	}
+  @Override
+  public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period,
+      TimeUnit unit) {
+    return scheduledExecutorService.scheduleAtFixedRate(command, initialDelay, period, unit);
+  }
 
-	@Override
-	public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay,
-			long delay, TimeUnit unit) {
-		return scheduledExecutorService.scheduleWithFixedDelay(command, initialDelay, delay, unit);
-	}
+  @Override
+  public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay,
+      TimeUnit unit) {
+    return scheduledExecutorService.scheduleWithFixedDelay(command, initialDelay, delay, unit);
+  }
 
-	public void addMinuteTasks(Runnable command) {
-		scheduledExecutorService.scheduleWithFixedDelay(command, 1, 1, TimeUnit.MINUTES);
-	}
+  public void addMinuteTasks(Runnable command) {
+    scheduledExecutorService.scheduleWithFixedDelay(command, 1, 1, TimeUnit.MINUTES);
+  }
 
-	public void addHourTask(Runnable command) {
-		scheduledExecutorService.scheduleWithFixedDelay(command, 1, 1, TimeUnit.HOURS);
-	}
+  public void addHourTask(Runnable command) {
+    scheduledExecutorService.scheduleWithFixedDelay(command, 1, 1, TimeUnit.HOURS);
+  }
 
-	public void addDailyTask(Runnable command) {
-		scheduledExecutorService.scheduleWithFixedDelay(command, 1, 1, TimeUnit.DAYS);
-	}
+  public void addDailyTask(Runnable command) {
+    scheduledExecutorService.scheduleWithFixedDelay(command, 1, 1, TimeUnit.DAYS);
+  }
 
-	public void addWeeklyTask(Runnable command) {
-		scheduledExecutorService.scheduleWithFixedDelay(command, 7, 7, TimeUnit.DAYS);
-	}
+  public void addWeeklyTask(Runnable command) {
+    scheduledExecutorService.scheduleWithFixedDelay(command, 7, 7, TimeUnit.DAYS);
+  }
 }
