6a96818d19d9d6d5710f42cbc54cc052ac7f228f
Hisayori Noda <nodchip@gmail.com>
2018-10-23 00:19:19 +0900

    Pythonプロジェクトの設定を行った

--- filter_log.py
+++ filter_log.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import io
 import sys
 
 def main():
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    
     out = True
     last_line = ''
-    for line in sys.stdin:
-        if line.startswith("警告:") or line.startswith("重大:") or line.startswith("WARNING:") or line.startswith("SEVERE:"):
-			if 'ダウンロードに失敗しました' not in line:
-				out = True
-        elif line.startswith("情報:") or line.startswith("INFO:"):
-            out = False
-        if out:
-            print last_line.strip()
-        last_line = line
+    with io.open(sys.stdin.fileno(),'r',encoding='utf-8') as sin:
+        for line in sin:
+            line = line.strip()
+            if (' 警告 ' in line or ' 重大 ' in line) and 'ダウンロードに失敗しました' not in line:
+                out = True
+            elif ' 情報 ' in line:
+                out = False
+                
+            if out and last_line:
+                print(last_line)
+                
+            last_line = line
 
 if __name__ == '__main__':
     main()
