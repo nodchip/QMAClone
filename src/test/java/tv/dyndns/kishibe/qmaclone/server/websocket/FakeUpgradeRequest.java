30ff22465b22c7550b8f4045cfdf391636484e34
nodchip <nodchip@gmail.com>
2026-02-10 08:09:57 +0900

    Jetty 9.4系に合わせてWebSocketテストを修正

--- src/test/java/tv/dyndns/kishibe/qmaclone/server/websocket/FakeUpgradeRequest.java
+++ src/test/java/tv/dyndns/kishibe/qmaclone/server/websocket/FakeUpgradeRequest.java
@@ -3,9 +3,9 @@ package tv.dyndns.kishibe.qmaclone.server.websocket;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.jetty.websocket.api.UpgradeRequest;
+import org.eclipse.jetty.websocket.common.UpgradeRequestAdapter;
 
-public class FakeUpgradeRequest extends UpgradeRequest {
+public class FakeUpgradeRequest extends UpgradeRequestAdapter {
   public FakeUpgradeRequest() {
     super(
         "ws://kishibe.dyndns.tv/QMAClone/websocket/tv.dyndns.kishibe.qmaclone.client.packet.PacketMatchingData");
