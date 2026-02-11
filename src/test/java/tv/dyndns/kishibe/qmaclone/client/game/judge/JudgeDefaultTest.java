e7596547099d3630f42d9fdbb7e5390e975f7e9a
nodchip <nodchip@gmail.com>
2026-02-11 12:19:28 +0900

    client/game/judgeテストをJUnit5へ移行

--- src/test/java/tv/dyndns/kishibe/qmaclone/client/game/judge/JudgeDefaultTest.java
+++ src/test/java/tv/dyndns/kishibe/qmaclone/client/game/judge/JudgeDefaultTest.java
@@ -1,18 +1,15 @@
 package tv.dyndns.kishibe.qmaclone.client.game.judge;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-@RunWith(JUnit4.class)
 public class JudgeDefaultTest extends JudgeTestBase {
 	private JudgeDefault judge;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		super.setUp();
 		judge = new JudgeDefault();
--- src/test/java/tv/dyndns/kishibe/qmaclone/client/game/judge/JudgeTatoTest.java
+++ src/test/java/tv/dyndns/kishibe/qmaclone/client/game/judge/JudgeTatoTest.java
@@ -1,20 +1,17 @@
 package tv.dyndns.kishibe.qmaclone.client.game.judge;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import tv.dyndns.kishibe.qmaclone.client.constant.Constant;
 
-@RunWith(JUnit4.class)
 public class JudgeTatoTest extends JudgeTestBase {
 	private JudgeTato judge;
 
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception {
 		super.setUp();
 		judge = new JudgeTato();
