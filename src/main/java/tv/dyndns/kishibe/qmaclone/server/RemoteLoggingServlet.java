3afb8d69bccee71c0fe230b0c9e3d65ac25a5b9b
nodchip <nodchip@gmail.com>
2026-02-14 15:27:05 +0900

    remote_logging重複マッピングを解消

--- src/main/java/tv/dyndns/kishibe/qmaclone/server/RemoteLoggingServlet.java
+++ src/main/java/tv/dyndns/kishibe/qmaclone/server/RemoteLoggingServlet.java
@@ -1,11 +1,7 @@
 package tv.dyndns.kishibe.qmaclone.server;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.google.gwt.logging.server.RemoteLoggingServiceImpl;
 
 @SuppressWarnings("serial")
-@WebServlet(urlPatterns = { "/tv.dyndns.kishibe.qmaclone.QMAClone/remote_logging",
-    "/remote_logging" })
 public class RemoteLoggingServlet extends RemoteLoggingServiceImpl {
 }
