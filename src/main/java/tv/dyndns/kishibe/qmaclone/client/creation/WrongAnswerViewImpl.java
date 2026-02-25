8b5d2fa4343acf0e9f1b1b7bec81e5fa7766e473
nodchip <nodchip@gmail.com>
2026-02-25 22:12:53 +0900

    問題作成の誤答例UIを表カード形式に刷新

--- src/main/java/tv/dyndns/kishibe/qmaclone/client/creation/WrongAnswerViewImpl.java
+++ src/main/java/tv/dyndns/kishibe/qmaclone/client/creation/WrongAnswerViewImpl.java
@@ -4,20 +4,13 @@ import java.util.List;
 
 import tv.dyndns.kishibe.qmaclone.client.packet.PacketWrongAnswer;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
-import com.google.gwt.visualization.client.DataTable;
-import com.google.gwt.visualization.client.visualizations.corechart.ComboChart.Options;
-import com.google.gwt.visualization.client.visualizations.corechart.PieChart;
 
 public class WrongAnswerViewImpl extends Composite implements WrongAnswerView {
 
@@ -28,8 +21,6 @@ public class WrongAnswerViewImpl extends Composite implements WrongAnswerView {
 	}
 
 	@UiField
-	SimplePanel panel;
-	@UiField
 	HTML html;
 
 	public WrongAnswerViewImpl() {
@@ -38,38 +29,38 @@ public class WrongAnswerViewImpl extends Composite implements WrongAnswerView {
 
 	@Override
 	public void setAnswer(List<PacketWrongAnswer> wrongAnswers) {
-		setChart(wrongAnswers);
 		setHtml(wrongAnswers);
 	}
 
-	private void setChart(List<PacketWrongAnswer> wrongAnswers) {
-		DataTable data = DataTable.create();
-		data.addColumn(ColumnType.STRING, "誤解答");
-		data.addColumn(ColumnType.NUMBER, "回答数");
-		data.addRows(wrongAnswers.size());
-		for (int row = 0; row < wrongAnswers.size(); ++row) {
-			PacketWrongAnswer wrongAnswer = wrongAnswers.get(row);
-			data.setValue(row, 0, wrongAnswer.answer);
-			data.setValue(row, 1, wrongAnswer.count);
-		}
-
-		Options options = Options.create();
-		options.setWidth(780);
-		options.setHeight(400);
-		options.setTitle("誤解答例");
-
-		panel.setWidget(new PieChart(data, options));
-	}
-
 	private void setHtml(List<PacketWrongAnswer> wrongAnswers) {
-		// 文字列化
-		List<String> formatteds = Lists.newArrayList();
-		for (PacketWrongAnswer wrongAnswer : wrongAnswers) {
-			formatteds.add(wrongAnswer.answer + "(" + wrongAnswer.count + ")");
+		StringBuilder builder = new StringBuilder();
+		if (wrongAnswers.isEmpty()) {
+			builder.append("<div class='creationWrongAnswerEmpty'>まだ誤答例はありません。</div>");
+			html.setHTML(builder.toString());
+			return;
 		}
 
-		String joined = Joiner.on(' ').join(formatteds);
-		html.setHTML(SafeHtmlUtils.fromString(joined));
+		builder.append("<div class='creationWrongAnswerBody'>");
+		builder.append("<table class='creationWrongAnswerTable'>");
+		builder.append(
+				"<thead><tr><th class='creationWrongAnswerRank'>順位</th><th>誤答</th><th class='creationWrongAnswerCount'>件数</th></tr></thead>");
+		builder.append("<tbody>");
+		for (int i = 0; i < wrongAnswers.size(); ++i) {
+			PacketWrongAnswer wrongAnswer = wrongAnswers.get(i);
+			builder.append("<tr>");
+			builder.append("<td class='creationWrongAnswerRank'>");
+			builder.append(i + 1);
+			builder.append("</td>");
+			builder.append("<td class='creationWrongAnswerAnswer'>");
+			builder.append(SafeHtmlUtils.htmlEscape(wrongAnswer.answer));
+			builder.append("</td>");
+			builder.append("<td class='creationWrongAnswerCount'>");
+			builder.append(wrongAnswer.count);
+			builder.append("</td>");
+			builder.append("</tr>");
+		}
+		builder.append("</tbody></table></div>");
+		html.setHTML(builder.toString());
 	}
 
 }
